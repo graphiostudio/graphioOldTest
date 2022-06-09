78cf17b9ccc6bd5d953008ea52ea7ad5522b7dbc
Alyn <alyn.a@hotmail.com>
2022-06-09 10:46:26 +0700

    Added loading page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { NavigationProvider } from "./context/navigation/NavigationContext";
-import { GalleryProvider } from "./context/gallery/GalleryContext";
-import { TranslationProvider } from "./context/translation/TranslationContext";
-import { ScrollToTop } from "./hooks/ScrollToTop";
-import Navigation from "./components/layouts/Navigation";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import Services from "./pages/Services";
-import Work from "./pages/Work";
-import Portfolio from "./pages/Portfolio";
-import NotFound from "./pages/NotFound";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { NavigationProvider } from './context/navigation/NavigationContext';
+import { GalleryProvider } from './context/gallery/GalleryContext';
+import { TranslationProvider } from './context/translation/TranslationContext';
+import { ScrollToTop } from './hooks/ScrollToTop';
+import Navigation from './components/layouts/Navigation';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Work from './pages/Work';
+import Portfolio from './pages/Portfolio';
+import NotFound from './pages/NotFound';
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
 function App() {
-  return (
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoaded(true), 3500);
+  }, []);
+
+  return isLoaded ? (
     <>
       <TranslationProvider>
         <NavigationProvider>
@@ -33,6 +41,37 @@ function App() {
         </NavigationProvider>
       </TranslationProvider>
     </>
+  ) : (
+    <div className="bg-neutral-800 h-screen grid">
+      <div className="place-self-center flex flex-col items-center">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.4, delay: 2.3 }}
+          className="lds-ripple"
+        >
+          <div></div>
+          <div></div>
+        </motion.div>
+        <p className="text-neutral-100 font-ginger mt-3">
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 0.8 }}
+          >
+            graphio
+          </motion.span>
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 1.8 }}
+          >
+            {' '}
+            creative studio
+          </motion.span>
+        </p>
+      </div>
+    </div>
   );
 }
 
